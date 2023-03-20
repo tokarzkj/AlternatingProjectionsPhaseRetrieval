98569988a4c0650a1027a8d26ce3b066d349b8aa
Kris Tokarz <kj.tokarz@gmail.com>
2023-03-20 16:34:12 -0400

    calculate basic measurement matrix for the masked fourier calc

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import numpy as np
+import scipy
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +8,22 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    N = 5        # N Samples
+    m = 4*N
+    numberIterations = 600
+
+    x = np.random.rand(N) + 1J*np.random.rand(N)
+
+    A = np.zeros((m, N), dtype=np.complex_)
+
+    diag = np.zeros((N, N))
+    for i in range(0, N):
+        diag[i][i] = 1
+
+    for i in range(0, int(m/N)):
+
+        A[i * N: (i * N) + N] = np.matmul(scipy.linalg.dft(N), diag * (np.random.rand(N) + 1J * np.random.rand(N)))
+
+    inverse_A = scipy.linalg.pinv(A)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
