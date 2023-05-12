20c54d88d6319191d726f5b25de2bfb2255ae926
Kris Tokarz <kj.tokarz@gmail.com>
2023-05-12 14:09:01 -0400

    Refactor tab1 into a separate class

--- UI.py
+++ UI.py
@@ -14,7 +14,12 @@ class MainPage(QtWidgets.QTabWidget):
     def __init__(self, parent=None):
         super().__init__()
 
-        self.alternating_project_phase_retrieval_tab = QWidget()
+        self.alternating_project_phase_retrieval_tab = AlternatingProjectTab()
+        self.addTab(self.alternating_project_phase_retrieval_tab, "Alternating Projection")
+
+class AlternatingProjectTab(QWidget):
+    def __init__(self, parent=None):
+        super().__init__()
 
         self.trials_window = None
         self.samples_label = QtWidgets.QLabel('Sample Count')
@@ -54,22 +59,21 @@ class MainPage(QtWidgets.QTabWidget):
         self.trials_button = QtWidgets.QPushButton('Calc Trials')
         self.trials_button.clicked.connect(self.trials)
 
-        self.alternating_project_phase_retrieval_tab.layout = QtWidgets.QGridLayout(self)
-        self.alternating_project_phase_retrieval_tab.layout.addWidget(self.samples_label, 0, 0)
-        self.alternating_project_phase_retrieval_tab.layout.addWidget(self.samples_value, 0, 1)
-        self.alternating_project_phase_retrieval_tab.layout.addWidget(self.seed_label, 1, 0)
-        self.alternating_project_phase_retrieval_tab.layout.addWidget(self.seed_value, 1, 1)
-        self.alternating_project_phase_retrieval_tab.layout.addWidget(self.mask_label, 2, 0)
-        self.alternating_project_phase_retrieval_tab.layout.addWidget(self.mask_combo_box, 2, 1)
-        self.alternating_project_phase_retrieval_tab.layout.addWidget(self.trials_label, 3, 0)
-        self.alternating_project_phase_retrieval_tab.layout.addWidget(self.trials_value, 3, 1)
-        self.alternating_project_phase_retrieval_tab.layout.addWidget(self.snr_checkbox_label, 4, 0)
-        self.alternating_project_phase_retrieval_tab.layout.addWidget(self.snr_checkbox, 4, 1)
-        self.alternating_project_phase_retrieval_tab.layout.addWidget(self.graph_random_projection_button, 5, 0)
-        self.alternating_project_phase_retrieval_tab.layout.addWidget(self.modified_graph_random_projection_button, 6, 0)
-        self.alternating_project_phase_retrieval_tab.layout.addWidget(self.trials_button, 7, 0)
+        self.layout = QtWidgets.QGridLayout(self)
+        self.layout.addWidget(self.samples_label, 0, 0)
+        self.layout.addWidget(self.samples_value, 0, 1)
+        self.layout.addWidget(self.seed_label, 1, 0)
+        self.layout.addWidget(self.seed_value, 1, 1)
+        self.layout.addWidget(self.mask_label, 2, 0)
+        self.layout.addWidget(self.mask_combo_box, 2, 1)
+        self.layout.addWidget(self.trials_label, 3, 0)
+        self.layout.addWidget(self.trials_value, 3, 1)
+        self.layout.addWidget(self.snr_checkbox_label, 4, 0)
+        self.layout.addWidget(self.snr_checkbox, 4, 1)
+        self.layout.addWidget(self.graph_random_projection_button, 5, 0)
+        self.layout.addWidget(self.modified_graph_random_projection_button, 6, 0)
+        self.layout.addWidget(self.trials_button, 7, 0)
 
-        self.addTab(self.alternating_project_phase_retrieval_tab, "Alternating Projection")
 
     @QtCore.Slot()
     def graph(self):
