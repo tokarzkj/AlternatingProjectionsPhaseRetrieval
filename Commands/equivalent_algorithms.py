82336d720b4440eada0f023ac408c715af959be1
Kris Tokarz <kj.tokarz@gmail.com>
2023-06-22 06:18:50 -0400

    Add a basic CLI
    The UI is becoming more complex than anticipated when starting the project.
    Since I don't have the time to commit to building the best UI the command line
    acts as a nice backup. In addition, some commands don't need a UI and this will
    simplify delivering these commands.

---
+++ Commands/equivalent_algorithms.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+import measurement
+from utilities import perturb_vec
+
+
+def display_forward_and_backward_time_shift_equivalence():
+    N = 25
+    m = 8 * N
+    number_iterations = 600
+
+    x = np.random.rand(N) + 1J * np.random.rand(N)
+    mask = np.random.rand(N) + 1J * np.random.rand(N)
+    mask_estimate = perturb_vec(mask)
+    x_estimate = perturb_vec(x)
+
+    (_, x_recon, _, signal_error, signal_recon_iterations) = \
+        measurement.modified_alternating_phase_projection_recovery(N, m,
+                                                                   number_iterations,
+                                                                   0,
+                                                                   False,
+                                                                   x=x,
+                                                                   mask=mask)
+
+    (mask_recon, _, _) = \
+        measurement.modified_alternating_phase_projection_recovery_for_mask(mask, x, m, number_iterations,
+                                                                            False)
+
+    print('x_recon, mask_recon', 'x_recon - mask_recon')
+    for idx in range(0, len(x_recon)):
+        x_sample = x_recon[idx]
+        mask_sample = mask_recon[idx]
+
+        print('{:e}, {:e}, {:e}'.format(x_sample, mask_sample, x_sample - mask_sample))
--- main.py
+++ main.py
@@ -3,16 +3,22 @@ import sys
 from PySide6.QtWidgets import QApplication
 
 import UI
-
+from Commands.equivalent_algorithms import display_forward_and_backward_time_shift_equivalence
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    # Create and show the form
-    mainpage = UI.MainPage()
-    mainpage.resize(800, 800)
-    mainpage.show()
-    # Run the main Qt loop
-    sys.exit(app.exec())
+    print("Enter GUI or CLI?")
+    cmd = input()
+
+    if cmd == 'GUI':
+        app = QApplication(sys.argv)
+        # Create and show the form
+        mainpage = UI.MainPage()
+        mainpage.resize(800, 800)
+        mainpage.show()
+        # Run the main Qt loop
+        sys.exit(app.exec())
+    elif cmd == 'CLI':
+        display_forward_and_backward_time_shift_equivalence()
 
 
 
