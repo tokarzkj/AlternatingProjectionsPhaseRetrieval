Fix the error calculation for the mask recovery

Commands/equivalent_algorithms.py
@@ -12,7 +12,7 @@ def display_forward_and_backward_time_shift_equivalence():
     x = np.random.rand(N) + 1J * np.random.rand(N)
     mask = np.random.rand(N) + 1J * np.random.rand(N)
 
-    (_, x_recon, _, signal_error, _) = \
+    (_, x_recon, _, signal_error, signal_iterations) = \
         measurement.modified_alternating_phase_projection_recovery(N, m,
                                                                    number_iterations,
                                                                    False,
@@ -25,9 +25,9 @@ def display_forward_and_backward_time_shift_equivalence():
         x_recon_sample = x_recon[idx]
 
         print('{:e}, {:e}, {:e}'.format(x_sample, x_recon_sample, x_sample - x_recon_sample))
-    print('The overall error is {:e}'.format(signal_error))
+    print('The overall error for the signal recovery is {:e}'.format(signal_error))
 
-    (mask_recon, mask_error, _) = \
+    (mask_recon, mask_error, mask_iterations) = \
         measurement.modified_alternating_phase_projection_recovery_for_mask(mask, x, m, number_iterations,
                                                                             False)
 
@@ -40,4 +40,12 @@ def display_forward_and_backward_time_shift_equivalence():
         mask_recon_sample = mask_recon[idx]
 
         print('{:e}, {:e}, {:e}'.format(mask_sample, mask_recon_sample, mask_sample - mask_recon_sample))
-    print('The overall error is {:e}'.format(mask_error))
+    print('The overall error for the mask recovery is {:e}'.format(mask_error))
+
+    print("#############################################################################################")
+    print("#############################################################################################")
+    print("Iteration Number, Signal Iteration Err, Mask Iteration Err")
+    for k in signal_iterations.keys():
+        signal_iter_err = np.linalg.norm(x - signal_iterations[k]) / np.linalg.norm(x)
+        mask_iter_err = np.linalg.norm(mask - mask_iterations[k]) / np.linalg.norm(mask)
+        print('{:d}, {:e}, {:e}'.format(k, signal_iter_err, mask_iter_err))

measurement.py
@@ -1,6 +1,6 @@
 import numpy as np
 import scipy
-from numpy import imag
+from collections import OrderedDict
 
 from utilities import perturb_vec, simulate_noise_in_measurement, signum
 
@@ -154,11 +154,11 @@ def modified_alternating_phase_projection_recovery_for_mask(mask, x, m, number_i
                                                                         number_iterations)
 
     phasefac = np.matmul(np.conjugate(mask_recon).T, mask) / np.matmul(np.conjugate(mask).T, mask)
-    x_recon = np.multiply(mask_recon, signum(phasefac))
+    mask_recon = np.multiply(mask_recon, signum(phasefac))
 
-    error = np.linalg.norm(x - x_recon) / np.linalg.norm(x)
+    error = np.linalg.norm(mask - mask_recon) / np.linalg.norm(mask)
 
-    return x_recon, error, mask_recon_iterations
+    return mask_recon, error, mask_recon_iterations
 
 
 def create_measurement_matrix(m, N, vec, do_shift_left=False):
@@ -196,7 +196,7 @@ def iterative_signal_reconstruction(measurement_matrix, N, b, inverse_A, number_
 
 
 def reconstructed_signal(signal, A, b, inverse_A, number_iterations):
-    reconstructed_signal_iterations = dict()
+    reconstructed_signal_iterations = OrderedDict()
     for i in range(0, number_iterations):
         temp = np.array(list(map(signum, np.matmul(A, signal))), dtype=np.complex_)
         signal = np.matmul(inverse_A, np.multiply(b, temp))